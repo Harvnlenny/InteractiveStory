4dad6f7fd2bb0f177b533f964ea987460c4cadd2
Aaron Harvey <harvnlenny@gmail.com>
2015-11-13 20:13:49 -0500

    Initial commit

---
+++ app/src/main/java/com/harvnlenny/interactivestory/model/Choice.java
@@ -0,0 +1,30 @@
+package com.harvnlenny.interactivestory.model;
+
+/**
+ * Created by aaronharvey on 11/11/15.
+ */
+public class Choice {
+    private String mText;
+    private int mNextPage;
+
+    public Choice(String text, int nextPage) {
+        mText = text;
+        mNextPage = nextPage;
+    }
+
+    public String getText() {
+        return mText;
+    }
+
+    public void setText(String text) {
+        mText = text;
+    }
+
+    public int getNextPage() {
+        return mNextPage;
+    }
+
+    public void setNextPage(int nextPage) {
+        mNextPage = nextPage;
+    }
+}
